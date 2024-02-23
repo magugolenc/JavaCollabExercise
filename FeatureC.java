32dac2f53ca9c76097e77b6d26c7e78eb372e95b
IvanNaranjoEducaand <inarmar@g.educaand.es>
2024-02-23 14:15:05 +0100

    Feature C

---
+++ FeatureC.java
@@ -0,0 +1,12 @@
+public class FeatureC{
+	public static void main(String[] args){
+		int a = 7;
+		int b = 8;
+		
+		System.out.println("Primer numero: "+a);
+		System.out.println("Segundo numero: "+b);
+		int c = a * b;
+		System.out.println("Multiplicacion: "+c);
+
+	}
+}
